262710d047a68ff87aed131479b811aa39649656
potatochips178 <potatochips@sookmyung.ac.kr>
2021-01-09 17:42:59 +0900

    feat: common values

---
+++ app/src/main/java/com/example/timmo_songjas/feature/utils/CommonValues.java
@@ -0,0 +1,21 @@
+package com.example.timmo_songjas.feature.utils;
+
+public class CommonValues {
+
+    public static int SERVER_USERID ;
+    public static String SEVER_USERID_LOGGED_IN;
+    public static String USER_NAME_LOGGED_IN ;
+
+    //사용자 로그인 토큰큰
+   public static String USER_TOKEN;
+
+
+    //intent request code
+    public static final int PICK_FROM_ALBUM = 100;
+    public static final int PROADD_IMAGE = 200;
+    public static final int PROADD_1TO2 = 201;
+    public static final int PROADD_2TO3 = 202;
+    public static final int PARTY_TO_PROADD = 203;
+    public static final int PROFADD_IMAGE = 204;
+
+}
