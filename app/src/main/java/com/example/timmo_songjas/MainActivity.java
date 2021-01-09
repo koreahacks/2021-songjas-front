83ad0acf261beadd094dd898cd97a7f6aedca82c
eugene-tp <ttpp05152@sookmyung.ac.kr>
2021-01-09 11:49:07 +0900

    feat : add 메인 엑티비티 틀 작성, 프로젝트 파인드 페이지 생성, 툴바 생성(참고하세요)

--- app/src/main/java/com/example/timmo_songjas/MainActivity.java
+++ app/src/main/java/com/example/timmo_songjas/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.timmo_songjas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.timmo_songjas.feature.project.ProjectFindFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+
+    //연결한 프래그먼트
+    private ProjectFindFragment projectFindFragment = new ProjectFindFragment();
+    private ProjectFindFragment memberFindFragment = new ProjectFindFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        ((FragmentTransaction) transaction).replace(R.id.main_content, projectFindFragment).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+    }
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.cube_menu:
+                    transaction.replace(R.id.main_content, projectFindFragment).commitAllowingStateLoss();
+                    break;
+                case R.id.overlab_menu:
+                    transaction.replace(R.id.main_content, memberFindFragment).commitAllowingStateLoss();
+                    break;
+                case R.id.profile_menu:
+                    break;
+                case R.id.chat_menu:
+                    return true;
+            }
+            return true;
+        }
     }
 }
---
+++ app/src/main/java/com/example/timmo_songjas/feature/project/ProjectFindFragment.java
@@ -0,0 +1,83 @@
+package com.example.timmo_songjas.feature.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.timmo_songjas.MainActivity;
+import com.example.timmo_songjas.R;
+
+public class ProjectFindFragment extends Fragment {
+
+
+    private ImageView filter_btn;
+    private ImageView subtitle_projectfind;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container,
+                             @NonNull Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_project_find, container, false);
+
+        //툴바영역
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar_1); //fragment xml 툴바 영역?
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //프래그먼트에 키보드 입력 시 하단 올라오지 않게
+        ((MainActivity) getActivity()).getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
+        SearchView searchView = view.findViewById(R.id.search_view_projectfind);
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        //searchView.setIconifiedByDefault(false);
+
+        //SearchView의 검색 이벤트
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {  //키워드 입력 후 엔터 입력
+                Toast.makeText(getActivity(), query, Toast.LENGTH_SHORT).show();
+                //TODO: 네트워킹 필요
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) { //검색창에서 키워드 변경이 있을 때마다 호출
+                return false;
+            }
+        });
+
+        //리싸이클러뷰 어댑터 만들고 추가
+
+
+        //필터 버튼
+        filter_btn = (ImageView) toolbar.findViewById(R.id.filter_projectfind);
+        filter_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(getActivity(), FilterActivity.class);
+                //startActivityForResult(intent, 1004);
+            }
+        });
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+}
