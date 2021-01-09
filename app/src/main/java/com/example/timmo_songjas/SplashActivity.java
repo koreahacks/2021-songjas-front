16c6e638f3df06ad8e05229bbf74db9022852fc7
hyunJIN7 <yunjingong12@naver.com>
2021-01-09 13:20:59 +0900

    feat : SplashActivity add firebaseconfig

--- app/src/main/java/com/example/timmo_songjas/SplashActivity.java
+++ app/src/main/java/com/example/timmo_songjas/SplashActivity.java
@@ -1,14 +1,87 @@
 package com.example.timmo_songjas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.example.timmo_songjas.feature.sign.SignInActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private FirebaseRemoteConfig firebaseRemoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //원격 구성 코드
+        //https://firebase.google.com/docs/remote-config/use-config-android
+        firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(3600)
+                .build();
+        firebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+
+        firebaseRemoteConfig.setDefaultsAsync(R.xml.remote_config_defaults);
+
+        //서버에 값이 있을 경우
+        firebaseRemoteConfig.fetchAndActivate()
+                .addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Boolean> task) {
+                        if (task.isSuccessful()) {
+                            boolean updated = task.getResult();
+
+                            //아래 내용은 굳이 뭐 상관 없지만
+                            Log.d("SplashActivity", "Config params updated: " + updated);
+                            Toast.makeText(SplashActivity.this, "Fetch and activate succeeded",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(SplashActivity.this, "Fetch failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        displayMessage();
+                    }
+                });
     }
+
+    private void displayMessage() {
+        //default_config.xml 값 불러서
+        boolean caps = firebaseRemoteConfig.getBoolean("splash_message_caps");
+        String splash_message = firebaseRemoteConfig.getString("splash_message");
+
+        if (caps) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(splash_message).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+            builder.create().show(); //확인 누르면 꺼져야하니
+
+        } else {//정상
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
 }
