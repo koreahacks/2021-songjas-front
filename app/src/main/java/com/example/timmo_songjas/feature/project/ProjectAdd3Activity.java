6261e5db83102725cc2fa9c50c16ad821467bc2e
potatochips178 <potatochips@sookmyung.ac.kr>
2021-01-09 13:15:39 +0900

    feat: project add3 xml

---
+++ app/src/main/java/com/example/timmo_songjas/feature/project/ProjectAdd3Activity.java
@@ -0,0 +1,16 @@
+package com.example.timmo_songjas.feature.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.timmo_songjas.R;
+
+public class ProjectAdd3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_add3);
+    }
+}
